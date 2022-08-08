b0cd47a3bea0fe32997f96b1f0d2973cbb62fc19
boccanera <nailtonboccanera@gmail.com>
2022-08-08 19:16:43 -0300

    postgresql configurado para localhostxs

--- src/main/java/com/example/demo/configuration/MainConfig.java
+++ src/main/java/com/example/demo/configuration/MainConfig.java
@@ -15,26 +15,36 @@ import java.net.URISyntaxException;
 @Configuration
 public class MainConfig {
 
+
         @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://ec2-34-235-31-124.compute-1.amazonaws.com:5432/d1sq20idpg7m85?sslmode=require");
-        dataSource.setUsername("ctgauoicjumyke");
-        dataSource.setPassword("e54c7de3ede02829663a63a082a296c4ecc33f705ec25cb132f09b69b42224c4");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("password1234");
         return dataSource;
     }
-    {}
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.POSTGRESQL);
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
-        adapter.setPrepareConnection(true);
-        return adapter;
-    }
+//        @Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://ec2-34-235-31-124.compute-1.amazonaws.com:5432/d1sq20idpg7m85?sslmode=require");
+//        dataSource.setUsername("ctgauoicjumyke");
+//        dataSource.setPassword("e54c7de3ede02829663a63a082a296c4ecc33f705ec25cb132f09b69b42224c4");
+//        return dataSource;
+//    }
+//    {}
+//    @Bean
+//    public JpaVendorAdapter jpaVendorAdapter(){
+//        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//        adapter.setDatabase(Database.POSTGRESQL);
+//        adapter.setShowSql(true);
+//        adapter.setGenerateDdl(true);
+//        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+//        adapter.setPrepareConnection(true);
+//        return adapter;
+//    }
 //
 //    @Bean
 //    public BasicDataSource dataSource() throws URISyntaxException {
