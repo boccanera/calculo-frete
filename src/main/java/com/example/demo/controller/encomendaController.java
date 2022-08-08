344de660f2bd8c41121e5445c1bc351f305df834
boccanera <nailtonboccanera@gmail.com>
2022-08-08 16:30:47 -0300

    11 commit

--- src/main/java/com/example/demo/controller/encomendaController.java
+++ src/main/java/com/example/demo/controller/encomendaController.java
@@ -10,12 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.text.DecimalFormat;
 import java.util.Optional;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class encomendaController {
 
